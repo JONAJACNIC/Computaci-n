24cbd5ba2801e3ab596c2a4a9f705d8d3e98959b
Javier23400 <152934222+Javier23400@users.noreply.github.com>
2023-12-25 18:59:12 -0500

    Recuperar_Contrasena.php

---
+++ seguridad/Recuperar_Contrasena.php
@@ -0,0 +1,66 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'mail_reset.php'; 
+include 'conexion.php'; // Ajusta el nombre del archivo según tu estructura
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Recuperar el nombre de usuario del formulario
+    $username = $_POST['username'];
+
+    // Consultar la base de datos para obtener la información del usuario
+    $sql = "SELECT * FROM login WHERE log_usuario = '$username'";
+    $result = mysqli_query($conn, $sql);
+
+    if ($result) {
+        $usuario = mysqli_fetch_assoc($result);
+
+        // Configurar PHPMailer
+        $mail = new PHPMailer(true);
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'pedrosua141@gmail.com'; // Tu dirección de correo electrónico de Gmail
+        $mail->Password = 'edwwbjolswgipymz'; // Tu contraseña de Gmail
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
+
+        // Configurar el contenido del correo
+        $mail->setFrom('pedrosua141@gmail.com', 'Tu Nombre');
+        $mail->addAddress('pedrosua141@gmail.com', 'Tu Nombre');
+        $mail->isHTML(true);
+        $mail->Subject = 'Recuperación de Contraseña';
+        $mail->Body = 'Usuario: ' . $usuario['username'] . '<br>Contraseña: ' . $usuario['password'];
+
+        // Enviar el correo
+        try {
+            $mail->send();
+            echo 'Correo enviado correctamente';
+        } catch (Exception $e) {
+            echo 'Error al enviar el correo: ', $mail->ErrorInfo;
+        }
+    } else {
+        echo 'Error al consultar la base de datos: ', mysqli_error($conn);
+    }
+}
+
+// Cerrar la conexión a la base de datos
+mysqli_close($conn);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recuperar Contraseña</title>
+</head>
+<body>
+    <form action="recuperar_contrasena.php" method="post">
+        <label for="username">Nombre de Usuario:</label>
+        <input type="text" id="username" name="username" required>
+        <button type="submit">Recuperar Contraseña</button>
+    </form>
+</body>
+</html>
