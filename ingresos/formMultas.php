ee77281deac50a8da0909de7f41adcbbe9113305
ThomyEp <104998274+ThomyEp@users.noreply.github.com>
2023-12-26 13:55:39 -0500

    Add files via upload

---
+++ ingresos/formMultas.php
@@ -0,0 +1,229 @@
+<?php
+session_start();
+include('../buscar/guardar_dato.php');
+include("../conexion.php");
+include('consultaTipoMult.php');
+$opcionesTipoMulta = obtenerOpcionesTipoMulta();
+$fechaActual = date("Y-m-d");
+$cta_sc_id = 0;
+$total_multas = "";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="../style.css">
+</head>
+
+<body>
+  <div class="container-fuid">
+    <h5 class="text-center">Multas</h5>
+
+    <form action="" method="post">
+      <!-- Buscar socio -->
+      <div class="container-fluid">
+        <div class="card">
+          <div class="card-header fw-bold">
+            Datos Socio
+          </div>
+          <div class="card-body">
+            <!-- Barra de busqueda -->
+            <div class="row mb-2">
+              <?php include('../buscar/buscar.php'); ?>
+              <?php
+              // Mostrar el dato seleccionado
+              if (!empty($_SESSION['datos_formulario'])) {
+                $datosFormulario = $_SESSION['datos_formulario'];
+                unset($_SESSION['datos_formulario']);
+              }
+              ?>
+              <!-- Fila 1 -->
+
+              <!-- Campo nombres -->
+              <div class="col-md-3 d-flex align-items-center">
+                <label for="nombre" class="form-label">Nombres</label>
+                <input type="text" class="form-control" name="nombre" id="nombre" value="<?php echo isset($datosFormulario['nombre']) ? htmlspecialchars($datosFormulario['nombre']) : ''; ?>" readonly>
+              </div>
+              <!-- fin campo nombres -->
+              <!-- Campo apellidos -->
+              <div class="col-md-3 d-flex align-items-center">
+                <label for="apellido" class="form-label">Apellidos</label>
+                <input type="text" class="form-control" name="apellido" id="apellido" value="<?php echo isset($datosFormulario['apellido']) ? htmlspecialchars($datosFormulario['apellido']) : ''; ?>" readonly>
+              </div>
+              <!-- fin campo apellidos -->
+              <!-- Campo cédula -->
+              <div class="col-md-2 d-flex align-items-center">
+                <label for="cedula" class="form-label ">Cédula</label>
+                <input type="text" class="form-control" name="cedula" id="cedula" value="<?php echo isset($datosFormulario['cedula']) ? htmlspecialchars($datosFormulario['cedula']) : ''; ?>" required readonly>
+              </div>
+              <!-- fin campo cédula -->
+            </div>
+            <!--  fin barra de busqueda -->
+          </div>
+        </div>
+        <!-- Fin buscar socio -->
+        <!-- Multas -->
+        <div class="card">
+          <div class="card-header fw-bold">
+            Registro Multas
+          </div>
+          <div class="card-body">
+            <div class="col-md-3 d-flex align-items-center">
+              <label for="tipoMulta" class="form-label">Tipo de Multa</label>
+              <select name="tipoMulta" id="tipoMulta" class="form-select">
+                <option value=""> ---- </option>
+                <?php
+                // Consulta para obtener los datos de la tabla tipo_multa
+                $sql = "SELECT pk_tp_mult_id, mult_det, mult_val FROM tipo_multa;";
+                $result = $conn->query($sql);
+
+                // Generar las opciones dinámicamente
+                while ($row = $result->fetch_assoc()) {
+                  echo "<option value='{$row['mult_val']}'>{$row['mult_det']}</option>";
+                }
+                ?>
+              </select>
+            </div>
+
+            <!-- Botón "Agregar Multa" sin enviar el formulario -->
+            <center><button type="button" class="btn btn-outline-success" id="btnAgregarMulta">Agregar Multa</button></center>
+          </div>
+        </div>
+        <!-- fin registro multas -->
+
+        <!-- Tabla de Multas -->
+        <div class="card">
+          <div class="card-header fw-bold">
+            Detalles de Multas
+          </div>
+          <div class="card-body">
+            <table id="tablaMultas">
+              <thead>
+                <tr>
+                  <th>Tipo Multa</th>
+                  <th>Valor</th>
+                  <th>Pago</th>
+                </tr>
+              </thead>
+              <tbody></tbody>
+            </table>
+          </div>
+        </div>
+        <!-- Fin Tabla de Multas -->
+
+        <!-- Valores a cancelar -->
+        <div class="card">
+          <div class="card-header fw-bold">
+            Información de pago
+          </div>
+          <div class="card-body ">
+            <div class="row mb-2">
+              <!-- fecha pago -->
+              <div class="col-md-3 d-flex ">
+                <label for="fechap" class="form-label me-5">Fecha</label>
+                <input type="date" class="form-control w-50" name="fechap" id="fechap" value="<?php echo date("Y-m-d"); ?>" readonly>
+              </div>
+              <!-- fin fecha pago -->
+              <!-- multas -->
+              <div class="col-md-3 d-flex ">
+                <label for="multas" class="form-label me-5">Multas</label>
+                <input type="text" class="form-control w-50" name="multas" id="multas" value="<?php echo $total_multas; ?>" readonly>
+              </div>
+              <!-- fin multas -->
+              <!-- total a pagar -->
+              <div class="col-md-3 d-flex ">
+                <label for="total" class="form-label me-5">Total</label>
+                <input type="text" class="form-control w-50" name="total" id="total" readonly>
+              </div>
+              <!-- fin total a pagar -->
+              <input type="hidden" name="fechasSeleccionadas" id="fechasSeleccionadas" value="" />
+            </div>
+          </div>
+        </div>
+
+        <!-- fin valores a cancelar -->
+
+        <div class="py-2 text-center">
+          <input type="submit" value="Registrar" id="btnRegistrar" name="btnRegistrar" class="btn btn-outline-success">
+          <button type="button" class="btn btn-outline-success">Imprimir</button>
+        </div>
+    </form>
+  </div>
+
+  <!-- Scripts -->
+  <script>
+    document.addEventListener("DOMContentLoaded", function() {
+      var cantidadMultas = 0;
+
+      function agregarMulta() {
+        // Evitar que el formulario se envíe automáticamente al hacer clic en el botón
+        event.preventDefault();
+
+        var tipoMultaSelect = document.getElementById("tipoMulta");
+        var tipoMultaOption = tipoMultaSelect.options[tipoMultaSelect.selectedIndex];
+
+        if (tipoMultaOption) {
+          // Obtener el valor de "mult_val" del formato "mult_val (valor)"
+          var multaValue = parseFloat(tipoMultaOption.value);
+
+
+          // Crear una nueva fila en la tabla
+          var tabla = document.getElementById("tablaMultas");
+          var nuevaFila = tabla.insertRow(-1);
+
+          // Crear celdas para cada columna
+          var celdaTipoMulta = nuevaFila.insertCell(0);
+          var celdaValorMulta = nuevaFila.insertCell(1);
+          var celdaPago = nuevaFila.insertCell(2);
+
+          // Asignar valores a las celdas
+          celdaTipoMulta.textContent = tipoMultaOption.text;
+          celdaValorMulta.textContent = multaValue.toFixed(2);
+
+          // Agregar checkbox para el pago
+          var checkboxPago = document.createElement("input");
+          checkboxPago.type = "checkbox";
+          checkboxPago.name = "Pago[]";
+          checkboxPago.value = multaValue;
+          checkboxPago.addEventListener("change", actualizarTotal);
+          celdaPago.appendChild(checkboxPago);
+
+          // Actualizar el valor del campo "Total"
+          actualizarTotal();
+
+          // Contar la cantidad de multas y actualizar el campo correspondiente
+          cantidadMultas++;
+          document.getElementById("cantidadMultas").value = cantidadMultas;
+
+          // Limpiar el campo "Tipo de Multa" para futuras selecciones
+          tipoMultaSelect.value = "";
+        } else {
+          alert("Selecciona un tipo de multa antes de agregar.");
+        }
+      }
+
+      function actualizarTotal() {
+        var totalInput = document.getElementById("total");
+        var totalActual = 0;
+        var checkboxesSeleccionados = document.querySelectorAll('input[name="Pago[]"]:checked');
+
+        checkboxesSeleccionados.forEach(function(checkbox) {
+          var valorMulta = parseFloat(checkbox.value);
+          totalActual += valorMulta;
+        });
+
+        totalInput.value = totalActual.toFixed(2);
+      }
+
+      // Obtener el botón "Agregar Multa" y asociar la función agregarMulta al evento click
+      var btnAgregarMulta = document.getElementById("btnAgregarMulta");
+      btnAgregarMulta.addEventListener("click", agregarMulta);
+    });
+  </script>
+</body>
+
+</html>
---
+++ ingresos/formPagNueSc.php
@@ -0,0 +1,183 @@
+<?php
+include("../conexion.php");
+include('regtPagNueSc.php');
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pago Nuevo Socio</title>
+    <link rel="stylesheet" href="../style.css">
+    <style>
+        #sugerencias {
+            position: absolute;
+            top: 90%;
+            left: 24;
+            z-index: 9999;
+            /* Ajusta el valor según sea necesario, asegúrate de que sea mayor que cualquier otro z-index en tu página */
+            background-color: #fff;
+            border: 1px solid #ccc;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+            max-height: 200px;
+            overflow-y: auto;
+            width: 40%;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <form action="" method="post" id="miFormulario">
+            <div class="card">
+                <div class="card-header fw-bold">
+                    Datos Nuevo Socio
+                </div>
+                <div class="card-body">
+                    <!-- Barra de búsqueda -->
+                    <div class="row mb-2">
+                        <!-- Campo buscar -->
+                        <div class="col-md-3 d-flex align-items-center">
+                            <label for="buscar" class="form-label">Buscar</label>
+                            <input type="text" class="form-control" name="buscar" id="buscar" autocomplete="off">
+                            <ul id="sugerencias" class="sugerencias-container" style="display: none;"></ul>
+                            <p id="mensajeBuscarError"></p>
+                        </div>
+                        <!-- fin campo buscar -->
+                        <!-- Campo id socio -->
+                        <input type="hidden" class="form-control" name="idSoc" id="idSoc" required readonly>
+                        <!-- fin campo id socio -->
+                        <!-- Campo cuentaVal -->
+                        <input type="hidden" class="form-control" name="cuentaVal" id="cuentaVal" required readonly>
+                        <!-- fin campo cuentaVal -->
+                        <!-- Campo nombres -->
+                        <div class="col-md-4 d-flex align-items-center">
+                            <label for="nombre" class="form-label me-3">Nombres</label>
+                            <input type="text" class="form-control" name="nombre" id="nombre" readonly>
+                        </div>
+                        <!-- fin campo nombres -->
+                        <!-- Campo apellidos -->
+                        <div class="col-md-4 d-flex align-items-center ">
+                            <label for="apellido" class="form-label">Apellidos</label>
+                            <input type="text" class="form-control" name="apellido" id="apellido" readonly>
+                        </div>
+                        <!-- fin campo apellidos -->
+                        <!-- Campo cédula -->
+                        <div class="col-md-3 d-flex align-items-center mt-2">
+                            <label for="cedula" class="form-label ">Cédula</label>
+                            <input type="text" class="form-control" name="cedula" id="cedula" required readonly>
+                        </div>
+                        <!-- fin campo cédula -->
+                        <!-- Campo tipo socio -->
+                        <div class="col-md-3 d-flex align-items-center">
+                            <label for="tpSoc" class="form-label w-50">Tipo Socio</label>
+                            <input name="tpSoc" id="tpSoc" class="form-control" readonly required>
+                        </div>
+                        <!-- fin campo tipo socio -->
+                    </div>
+                    <!-- fin barra de búsqueda -->
+                </div>
+            </div>
+            <!-- Valores detallados -->
+            <div class="card">
+                <div class="card-header fw-bold">
+                    Valores Detallados
+                </div>
+                <div class="card-body">
+                    <!-- Fila 1 -->
+                    <div class="row mb-2">
+
+                        <!-- Campo valor Certificados Aportación -->
+                        <div class="col-md-3 d-flex align-items-center">
+                            <label for="certApor" class="form-label w-100">Certificados Aportación</label>
+                            <input type="text" class="form-control" name="certApor" id="certApor" readonly>
+                        </div>
+                        <!-- fin campo valor Certificados Aportación -->
+                        <!-- Campo valor Fondo Estratégico -->
+                        <div class="col-md-3 d-flex align-items-center">
+                            <label for="fondEst" class="form-label w-100">Fondo Estratégico</label>
+                            <input type="text" class="form-control" name="fondEst" id="fondEst" autocomplete="off" readonly>
+                        </div>
+                        <!-- fin campo valor Fondo Estratégico -->
+                        <!-- Campo valor Gastos Administrativos -->
+                        <div class="col-md-3 d-flex align-items-center">
+                            <label for="gastAdm" class="form-label w-100">Gastos Administrativos</label>
+                            <input type="text" class="form-control" name="gastAdm" id="gastAdm" readonly>
+                        </div>
+                        <!-- fin campo valor Gastos Administrativos -->
+                    </div>
+                    <!-- fin fila 1 -->
+                </div>
+            </div>
+            <!-- fin valores detallados -->
+            <!-- Información montos pendientes -->
+            <div class="card">
+                <div class="card-header fw-bold">
+                   Información de Pago
+                </div>
+                <div class="card-body">
+                    <!-- Fila 1 -->
+                    <div class="row mb-2">
+                        <!-- Campo valor total pendiente -->
+                        <div class="col-md-3 d-flex align-items-center">
+                            <label for="valPen" class="form-label w-100">Total a Pagar</label>
+                            <input type="text" class="form-control" name="valPen" id="valPen" disabled>
+                        </div>
+                        <!-- fin campo valor total pendiente -->
+                        <!-- Campo fecha actual-->
+                        <div class="col-md-3 d-flex align-items-center">
+                            <label for="fechaAct" class="form-label w-100 ">Fecha Pago</label>
+                            <input type="text" class="form-control" name="fechaAct" id="fechaAct" readonly>
+                        </div>
+                        <!-- fin campo fecha actual- -->
+                    </div>
+                    <!-- fin fila 1 -->
+
+                </div>
+            </div>
+            <!-- Fin información montos pendientes  -->
+            <div class="text-center m-1">
+                <input type="submit" value="Registrar" id="btnRegistrar" name="btnRegistrar" class="btn btn-outline-success">
+                <input type="submit" value="Limpiar" class="btn btn-outline-success">
+                <input type="submit" value="Imprimir" class="btn btn-outline-success">
+                <input type="hidden" value="Eliminar" class="btn btn-outline-success">
+            </div>
+        </form>
+        <!-- Contenedor de Alertas -->
+        <div class="mt-1 d-flex justify-content-center">
+            <?php
+            // Verifica si hay mensajes de éxito o error en la variable de sesión
+            if (isset($_SESSION['mensajeExito'])) {
+                echo '<div id="toastExito" class="w-25 toast align-items-center text-white bg-success border-0" role="alert" aria-live="assertive" aria-atomic="true">' .
+                    '<div class="d-flex justify-content-center align-items-center">' . // Añadida clase "justify-content-center align-items-center"
+                    '<div class="toast-body text-center">' . // Añadida clase "text-center"
+                    '<img src="../iconos/green-checkmark-icon.svg" alt="Icono de Socio" class="me-2" style="width: 24px; height: 24px;" />' .
+                    $_SESSION['mensajeExito'] .
+                    '</div>' .
+                    '<button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>' .
+                    '</div>' .
+                    '</div>';
+                unset($_SESSION['mensajeExito']); // Limpiar la variable de sesión
+            }
+            ?>
+        </div>
+    </div>
+    <!-- Fin Sección -->
+    <!-- Fin buscar socio -->
+    <script src="../node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="../node_modules/autonumeric/dist/autoNumeric.min.js"></script>
+    <script src="../ingresos/valdPagNueSc.js"></script>
+    <script>
+        $(document).ready(function() {
+            $("#toastExito").toast("show");
+            $("#toastError").toast("show");
+            deshabilitarCamposExcepto("buscar", "idSoc");
+
+        });
+    </script>
+</body>
+
+</html>
