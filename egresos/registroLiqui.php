82f3eca4b802b6597c4184b60b551bed467e4f1a
ThomyEp <104998274+ThomyEp@users.noreply.github.com>
2024-01-03 08:43:59 -0500

    Update registroLiqui.php

--- egresos/registroLiqui.php
+++ egresos/registroLiqui.php
@@ -6,43 +6,46 @@ if (!isset($_SESSION)) {
 
 // Verificar que la solicitud sea de tipo POST y se haya enviado el formulario
 if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["btnRegistrar"])) {
-    // Validar y escapar los datos del formulario
+    // Validar y llamar  los datos del formulario
     $id = isset($_POST['idSoc']) ? intval($_POST['idSoc']) : 0;
     $fechaIni = isset($_POST['fechaSol']) ? $_POST['fechaSol'] : '';
-    $fechaDes = isset($_POST['fechaDes']) ? $_POST['fechaDes'] : '';
     $totalLi = $_POST['total'];
     $tpLiqui = $_POST['tpLiqui'];
+
     // Limpiar el valor eliminando caracteres no numéricos
     $totalLi = preg_replace("/[^0-9.]/", "", $totalLi);
     // Convertir a float
     $totalLi = floatval($totalLi);
 
-    // Verificar y formatear la fecha
-    $fechaDesFormateada = date('Y-m-d', strtotime($fechaDes));
-
+    // Iniciar la transacción
+    $conn->begin_transaction();
     // Sentencia SQL preparada para la inserción
-    $sql = "INSERT INTO egreso (egre_val, egre_fech_ini, egre_fdesm, fk_tp_egre_id, fk_sc_id) 
-            VALUES (?, ?, ?, ?, ?)";
+    $sql = "INSERT INTO egreso (egre_val, egre_fech_ini, fk_tp_egre_id, fk_sc_id) 
+            VALUES (?,  ?, ?, ?)";
 
     // Preparar la consulta
     $stmt = $conn->prepare($sql);
 
     // Vincular parámetros
-    $stmt->bind_param("dssii", $totalLi, $fechaIni, $fechaDesFormateada, $tpLiqui, $id);
+    $stmt->bind_param("dsii", $totalLi, $fechaIni, $tpLiqui, $id);
+
     // Ejecutar la consulta
-    if ($stmt->execute()) {
+    $ejecucionExitosa = $stmt->execute();
+
+    // Confirmar o revertir la transacción según la ejecución de la consulta
+    if ($ejecucionExitosa) {
+        $conn->commit();
         $mensajeExito = '¡Registro exitoso!';
         $_SESSION['mensajeExito'] = $mensajeExito; // Guardar mensaje en la variable de sesión
-        header("Location:formLiqui.php");
+        header("Location: formLiqui.php");
         exit();
     } else {
+        $conn->rollback();
         echo "Error al insertar en la base de datos: " . $stmt->error;
         echo "Valor de totalLi después de la inserción: " . $totalLi;
     }
-
     // Cerrar la declaración
     $stmt->close();
 }
-
 // Cerrar la conexión a la base de datos
 $conn->close();
